c98b86246686a3a2da262a71577e0685ac26bfc9
Darshan_Badadhe <darshanbadadhe72@gmail.com>
2024-11-30 23:19:34 +0530

    Fetch Movie Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const API_URL = "https://api.themoviedb.org/3"
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+
+    const {data: results } = await axios.get(`${API_URL}/discover/movie`, {
+
+      params:{
+        api_key: process.env.REACT_APP_MOVIE_API_KEY
+      }
+
+
+    })
+
+    setMovies(results);
+
+  }
+
+  useEffect (() =>{
+
+    fetchMovies();
+
+  }, [])
+
   return (
     <div className="App">
       <h1>Darshan</h1>
